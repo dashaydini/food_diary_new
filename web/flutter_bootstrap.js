66c94306f9c0c1d42447c685f362e2a823fa86c7
shay <dinishay@icloud.com>
2026-09-03 20:07:15 +0300

    Prevent stale installed web app releases

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,31 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+(async () => {
+  const reloadKey = 'bite-the-way-cache-refresh';
+  const hadController = 'serviceWorker' in navigator &&
+      navigator.serviceWorker.controller != null;
+
+  if ('serviceWorker' in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.map((registration) => registration.unregister()));
+  }
+
+  if ('caches' in window) {
+    const cacheNames = await caches.keys();
+    await Promise.all(
+      cacheNames
+          .filter((name) => name.startsWith('flutter-'))
+          .map((name) => caches.delete(name)),
+    );
+  }
+
+  if (hadController && sessionStorage.getItem(reloadKey) !== 'done') {
+    sessionStorage.setItem(reloadKey, 'done');
+    window.location.reload();
+    return;
+  }
+
+  sessionStorage.removeItem(reloadKey);
+  await _flutter.loader.load();
+})();
